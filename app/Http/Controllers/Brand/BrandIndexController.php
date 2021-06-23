b86c7e9d72c3ca5c39a6e99ae02e85f76835df87
alexpandiyansuccess <pandiyanalex003@gmail.com>
2021-06-23 09:36:53 +0530

    Update BrandIndexController.php

--- app/Http/Controllers/Brand/BrandIndexController.php
+++ app/Http/Controllers/Brand/BrandIndexController.php
@@ -21,7 +21,11 @@ class BrandIndexController extends Controller
     public function __invoke(Request $request)
     {
         $brand = Brands::paginate(10);
+
         return Brands::with('brandModels')->find($brand);
+
+        // return new BrandResource($brand);
+
 
     }
 }
